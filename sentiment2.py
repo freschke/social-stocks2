7b3f70a808edf7ed95d7bbd1bebf8a089bf98d92
freschke <94104480+freschke@users.noreply.github.com>
2022-12-31 00:39:54 +0100

    Add files via upload

--- sentiment2.py
+++ sentiment2.py
@@ -8,6 +8,7 @@ from plotly import graph_objs as go
 import nltk
 import praw
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import datetime
 
 nltk.download('vader_lexicon')
 
@@ -22,18 +23,22 @@ st.title("Social Stocks")
 
 default_stock_goes_here = "GME"
 user_stock = st.text_input("Search for a Stock", default_stock_goes_here)
-default_amount_comments = 1000
-user_comment_amount = st.text_input("How many comments to you want to analyze", default_amount_comments)
 
-subreddit = reddit.subreddit("wallstreetbets")
-df = pd.DataFrame()
-df['title'] = ""
+all_subreddits = reddit.subreddit('all')
+df = pd.DataFrame(columns=['title', 'date'])
 comments = set()
-for submission in subreddit.search(user_stock, limit=int(user_comment_amount)):
-    comments.add(submission.title)
+
+# Search all subreddits using the search function
+for submission in all_subreddits.search(query=user_stock, limit=1000):
+    comments.add((submission.title, datetime.datetime.fromtimestamp(submission.created_utc).strftime('%Y-%m-%d'), submission.subreddit.display_name))
 
 df = pd.DataFrame(comments)
-df = df.set_axis(['title'], axis=1, inplace=False)
+df = df.set_axis(['title', 'date', 'subreddit'], axis=1, inplace=False)
+
+st.write(f"Number of comments about "{default_stock_goes_here}" that have been analyzed: {len(comments)}")
+st.write(f"Date of the oldest comment: {df['date'].min()}")
+
+st.dataframe(df[:10])
 
 analyzer = SentimentIntensityAnalyzer()
 df['sentiment'] = 0.1
@@ -54,7 +59,20 @@ fig = go.Figure(go.Indicator(
     value=stock_sentiment,
     domain={'x': [0, 1], 'y': [0, 1]},
     delta={'reference': 0, 'position': "top"},
-    title={'text': "Reddit Sentiment"},
-    gauge={'axis': {'range': [-1, 1]}}))
+    title={'text': "Reddit Sentiment Score"},
+    gauge={
+        'axis': {'range': [-1, 1]},
+        'bar': {
+            'color': 'red' if stock_sentiment < 0 else 'green'
+        }
+    }))
 
 st.plotly_chart(fig)
+st.markdown(
+    f"""
+
+        The Reddit Sentiment Score represents how this Social Media platform is feeling about your stock.
+        It is between -1 (highly negative sentiment) and 1 (highly positive sentiment), while 0 is beeing neutral.
+
+    """
+)
